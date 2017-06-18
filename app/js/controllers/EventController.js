ba3496c673d6d1a09733e7e0ac0140b33166b510
zahodinikola <zahodinikola@gmail.com>
2017-06-18 09:48:35 +0300

    using $http promises

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,7 +1,7 @@
 'use strict'
 
 eventsApp.controller('EventController',
-        function EventController($scope, eventData) {
+        function EventController($scope, eventData, $log) {
 
             $scope.snippet = "<span style='color: red'>hi there</span>";
             $scope.boolValue = false;
@@ -9,36 +9,20 @@ eventsApp.controller('EventController',
             $scope.myclass = "blue";
             $scope.sortOrder = 'upVoteCount';
             $scope.buttonDisabled = true;
-
-            $scope.event = eventData.event;
             
+            eventData.getEvent()
+                .success(function(event) {
+                        $scope.event = event;
+                })
+                .error(function(data, status, headers, config) {
+                        $log.warn(data, status, headers(), config);
+                });
+        
+
             $scope.upVoteSession = function(session) {
                     session.upVoteCount++;
-                }
+                };
             $scope.downVoteSession = function(session) {
                     session.upVoteCount--;
-            }
+                };
         })
-// eventsApp.controller("EventController",
-//     function EventController($scope, eventData, $log) {
-
-//         $scope.orderby = "name";
-//         $scope.snippet = '<span style="color: red"> hi there </span>';
-//         $scope.boolValue = false;
-//         $scope.mystyle = { color: 'red' };
-//         $scope.myclass = "cyan";
-//         $scope.buttonDisabled = true;
-//         eventData.getEvent()
-//             .success(function(event) {$scope.event = event; })
-//             .error(function(data, status, headers, config) {
-//                     $log.warn(data, status, headers(), config);
-//                 });
-        
-//         $scope.upVoteSession = function (session) {
-//             session.upVoteCount++;
-//         }
-//         $scope.downVoteSession = function (session) {
-//             session.upVoteCount--;
-//         }
-//     }
-// );
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,50 +1,44 @@
-eventsApp.factory("eventData", function($http, $log) {
+eventsApp.factory("eventData", function($http) {
     return {
-        event: {
-                name: "Angular Boot Camp",
-                date: "1/1/2013",
-                time: "10:30 am",
-                location: {
-                    address: "Google Headquarter",
-                    city: "Mountain View",
-                    province: "CA"
-                },
-                imageUrl: './img/angularjs-logo.png',
-                sessions: [
-                    {
-                        name: "Directives Masterclass Introductory",
-                        creatorName: "Bob Smith",
-                        duration: 1,
-                        level: "advanced",
-                        abstract: "In this session you will learn ins and outs of direc...",
-                        upVoteCount: 0
-                    },
-                    {
-                        name: "Scopes for fun and profit",
-                        creatorName: "John Doe",
-                        duration: 2,
-                        level: "Introductory",
-                        abstract: "This session will take a closer look at scopes. Learn...",
-                        upVoteCount: 0
-                    },
-                    {
-                        name: "Well Behaved Controllers",
-                        creatorName: "Jane Doe",
-                        duration: 4,
-                        level: "Intermediate",
-                        abstract: "Controllers are the beginning of everything Angular does...",
-                        upVoteCount: 0
-                    }
-                ]
-            },
-        getEvent:   function(successcb) {
-                        $http({method: 'GET', url: '../app/data/event/1'}).
-                            success(function (data, status, headers, config) {
-                                successcb(data);
-                            }).
-                            error(function(data, status, headers, config) {
-                                $log.warn(data, status, headers(), config);
-                            });
+        // event: {
+        //         name: "Angular Boot Camp",
+        //         date: "1/1/2013",
+        //         time: "10:30 am",
+        //         location: {
+        //             address: "Google Headquarter",
+        //             city: "Mountain View",
+        //             province: "CA"
+        //         },
+        //         imageUrl: './img/angularjs-logo.png',
+        //         sessions: [
+        //             {
+        //                 name: "Directives Masterclass Introductory",
+        //                 creatorName: "Bob Smith",
+        //                 duration: 1,
+        //                 level: "advanced",
+        //                 abstract: "In this session you will learn ins and outs of direc...",
+        //                 upVoteCount: 0
+        //             },
+        //             {
+        //                 name: "Scopes for fun and profit",
+        //                 creatorName: "John Doe",
+        //                 duration: 2,
+        //                 level: "Introductory",
+        //                 abstract: "This session will take a closer look at scopes. Learn...",
+        //                 upVoteCount: 0
+        //             },
+        //             {
+        //                 name: "Well Behaved Controllers",
+        //                 creatorName: "Jane Doe",
+        //                 duration: 4,
+        //                 level: "Intermediate",
+        //                 abstract: "Controllers are the beginning of everything Angular does...",
+        //                 upVoteCount: 0
+        //             }
+        //         ]
+        //     },
+        getEvent:   function() {
+                        return $http({method: 'GET', url: './data/event/1.json'});
                     }
     };
 });
